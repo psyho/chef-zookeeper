6d3b4095e0f2cba00db390f2576c1ac97ed34d35
Adam Pohorecki <adam.pohorecki@llp.pl>
2012-11-05 12:32:05 +0100

    updated recipe to work with ey gentoo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['zookeeper']['version'] = "3.4.4"
-default['zookeeper']['root_dir'] = "/mnt/local/zookeeper"
-default['zookeeper']['config_dir'] = "/mnt/local/zookeeper/conf"
-default['zookeeper']['data_dir'] = "/mnt/local/zookeeper/data"
+default['zookeeper']['root_dir'] = "/var/lib/zookeeper"
+default['zookeeper']['config_dir'] = "/var/lib/zookeeper/conf"
+default['zookeeper']['data_dir'] = "/var/lib/zookeeper/data"
 default['zookeeper']['client_port'] = "2181"
 default['zookeeper']['myid'] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,87 +15,100 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-p = "/etc/portage/package.license"
-java_package = "dev-java/sun-jdk"
+if ['util'].include?(node[:instance_role])
+  include_recipe 'emerge'
 
-update_file "enable java" do
-  action :append
-  path p
-  body "#{java_package} dlj-1.1"
-  not_if "grep '#{java_package}' #{p}"
-end
+  p = "/etc/portage/package.license"
+  java_package = "dev-java/sun-jdk"
 
-package java_package do
-  action :install
-end
+  update_file "enable java" do
+    action :append
+    path p
+    body "#{java_package} dlj-1.1"
+    not_if "grep '#{java_package}' #{p}"
+  end
 
-app_root_dir = node['zookeeper']['root_dir']
-data_dir = node['zookeeper']['data_dir']
-config_dir = node['zookeeper']['config_dir']
-client_port = node['zookeeper']['client_port']
-myid = node['zookeeper']['myid']
-servers = node['zookeeper']['server_list']
+  package java_package do
+    action :install
+  end
 
-user "zookeeper" do
-  system true
-  shell "/bin/false"
-end
+  app_root_dir = node['zookeeper']['root_dir']
+  data_dir = node['zookeeper']['data_dir']
+  config_dir = node['zookeeper']['config_dir']
+  client_port = node['zookeeper']['client_port']
+  myid = node['zookeeper']['myid']
+  servers = node['zookeeper']['server_list']
 
-[app_root_dir, config_dir, data_dir].each do |dir|
-  directory dir do
+  user "zookeeper" do
+    system true
+    shell "/bin/false"
+  end
+
+  [app_root_dir, config_dir, data_dir].each do |dir|
+    directory dir do
+      owner "zookeeper"
+      group "zookeeper"
+      mode "0755"
+      action :create
+    end
+  end
+
+  cookbook_file "/tmp/zookeeper.tar.gz" do
+    user "root"
+    source "zookeeper-#{node.zookeeper.version}.tar.gz"
+  end
+
+  bash "untar zookeeper" do
+    user "root"
+    cwd "/tmp"
+    code %(tar -zxf /tmp/zookeeper.tar.gz)
+    not_if { File.exists? "/tmp/zookeeper-#{node.zookeeper.version}" }
+  end
+
+  bash "copy zookeeper root" do
+    user "root"
+    cwd "/tmp"
+    code "cp -r /tmp/zookeeper-#{node.zookeeper.version}/* #{app_root_dir}"
+  end
+
+  template_variables = {
+    :zookeeper_servers           => servers,
+    :zookeeper_data_dir          => data_dir,
+    :zookeeper_client_port       => client_port
+  }
+
+  %w{ configuration.xsl log4j.properties zoo.cfg }.each do |templ|
+    template "#{config_dir}/#{templ}" do
+      source "#{templ}.erb"
+      mode "0644"
+      owner "root"
+      group "root"
+      variables(template_variables)
+    end
+  end
+
+  template "#{config_dir}/myid" do
+    source "myid.erb"
+    mode "0644"
     owner "zookeeper"
     group "zookeeper"
+    variables({:myid => myid})
+  end
+
+  template "/etc/init.d/zookeeper" do
+    user "root"
     mode "0755"
-    action :create
+    source "init.erb"
+    variables :root_dir => app_root_dir
   end
-end
 
-cookbook_file "/tmp/zookeeper.tar.gz" do
-  user "root"
-  source "zookeeper-#{node.zookeeper.version}.tar.gz"
-end
-
-bash "untar zookeeper" do
-  user "root"
-  cwd "/tmp"
-  code %(tar -zxf /tmp/zookeeper.tgz)
-  not_if { File.exists? "/tmp/zookeeper-#{node.zookeeper.version}" }
-end
-
-bash "copy zookeeper root" do
-  user "root"
-  cwd "/tmp"
-  code "cp -r /tmp/zookeeper-#{node.zookeeper.version}/* #{app_root_dir}"
-end
-
-template_variables = {
-  :zookeeper_servers           => servers,
-  :zookeeper_data_dir          => data_dir,
-  :zookeeper_client_port       => client_port
-}
-
-%w{ configuration.xsl log4j.properties zoo.cfg }.each do |templ|
-  template "#{config_dir}/#{templ}" do
-    source "#{templ}.erb"
-    mode "0644"
-    owner "root"
-    group "root"
-    variables(template_variables)
+  bash "run zookeeper at startup" do
+    user "root"
+    code "rc-update add zookeeper default"
   end
-end
 
-template "#{config_dir}/myid" do
-  source "myid.erb"
-  mode "0644"
-  owner "zookeeper"
-  group "zookeeper"
-  variables({:myid => myid})
-end
-
-bash "restart zookeeper" do
-  user "root"
-  cwd "#{app_root_dir}"
-  code %(bin/zkServer.sh restart)
+  service "zookeeper" do
+    action :start
+  end
 end
